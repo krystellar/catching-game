c54b1cf4f26e91670442c5c565e45b9b0994bd8b
{krystellar} <krystel.zerep@gmail.com>
2024-12-01 01:49:45 +0800

    add

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import './style.css'
+import Phaser from 'phaser'
+
+const sizes = {
+  width: 500,
+  height: 500
+}
+
+const speedDown = 100
+
+class GameScene extends Phaser.Scene{
+  constructor(){
+    super("scene-game")
+  }
+
+  preload(){
+    this.load.image("bg", "/assets/bg.png")
+  }
+
+  create(){
+    this.add.image(0,0,"bg")
+  }
+}
+
+const config = {
+  type:Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics:{
+    default:"arcade",
+    arcade: {
+      gravity:{y:speedDown},
+      debug: true
+    }
+  },
+  scene:{GameScene}
+}
+
+const game = new Phaser.Game(config)
